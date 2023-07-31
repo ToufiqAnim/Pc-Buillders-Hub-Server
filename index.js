fea42be182834a74bc571d1af7c6dbe82858a214
ToufiqAnim <toufiq9610@gmail.com>
2023-07-31 20:23:11 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri =
+  "mongodb+srv://pcBuilders:pxWKm0daO9tsWalA@cluster0.okbnb.mongodb.net/pcBuilders?retryWrites=true&w=majority";
+const client = new MongoClient(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+const run = async () => {
+  try {
+    await client.connect();
+    const db = client.db("pcBuilders");
+    const productCollection = db.collection("products");
+    const categoryCollection = db.collection("categories");
+
+    app.get("/products", async (req, res) => {
+      const products = await productCollection.find({}).toArray();
+
+      res.send({ status: true, data: products });
+    });
+
+    app.get("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const products = await productCollection.findOne({
+        _id: new ObjectId(id),
+      });
+
+      res.send({ status: true, data: products });
+    });
+
+    app.get("/categories", async (req, res) => {
+      const categories = await categoryCollection.find({}).toArray();
+
+      res.send({ status: true, data: categories });
+    });
+
+    app.get("/productsCategory/:category", async (req, res) => {
+      const category = req.params.category;
+      const products = await productCollection.find({ category }).toArray();
+
+      res.send({ status: true, data: products });
+    });
+  } finally {
+  }
+};
+
+run().catch((err) => console.log(err));
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
